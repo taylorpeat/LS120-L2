2ce63e3d44a5c07819b6d7e0569087c489d14b46
Taylor Peat <taylorpeat@hotmail.com>
2016-03-18 00:20:53 -0400

    Rubocop changes

--- oo_rps.rb
+++ oo_rps.rb
@@ -1,15 +1,16 @@
 class Player
   include Comparable
 
-  HANDS = ['r', 'p', 's']
+  HANDS = %w(r p s).freeze
+ 
   attr_reader :name, :hand
 
-  def <=>(another)
-    if hand == 'r' && another.hand == 's' ||
-       hand == 'p' && another.hand == 'r' ||
-       hand == 's' && another.hand == 'p'
+  def <=>(other)
+    if hand == 'r' && other.hand == 's' ||
+       hand == 'p' && other.hand == 'r' ||
+       hand == 's' && other.hand == 'p'
       1
-    elsif hand == another.hand
+    elsif hand == other.hand
       0
     else
       -1
@@ -19,7 +20,7 @@ end
 
 class Human < Player
   def pick_hand
-    loop do  
+    loop do
       puts "\nPlease select one: (r/p/s)"
       @hand = gets.chomp.downcase
       break if Player::HANDS.include?(hand)
@@ -33,14 +34,14 @@ class Human < Player
   def get_name(player_name)
     return player_name if player_name
     system 'clear' or system 'cls'
-    print "Enter your name: "
+    print 'Enter your name: '
     gets.chomp
   end
 end
 
 class Computer < Player
   def initialize
-    @name = "The Great Computer"
+    @name = 'The Great Computer'
   end
 
   def pick_hand
@@ -65,9 +66,9 @@ class Game
       computer
     end
   end
-  
+
   def play
-    [human, computer].each {|player| player.pick_hand}
+    [human, computer].each(&:pick_hand)
     winner = compare_hands
     puts winner.is_a?(String) ? winner : game_recap(winner)
     puts "#{winner.name} wins!" if defined?(winner.name)
@@ -76,9 +77,9 @@ class Game
 
   def game_recap(winner)
     case winner.hand
-    when 'r' then "Rock smashes scissors!"
-    when 'p' then "Paper wraps rock!"
-    when 's' then "Scissors cuts paper!"
+    when 'r' then 'Rock smashes scissors!'
+    when 'p' then 'Paper wraps rock!'
+    when 's' then 'Scissors cuts paper!'
     end
   end
 
@@ -87,13 +88,11 @@ class Game
     loop do
       puts "\nWould you like to play again? (Y/N)"
       continue = gets.chomp.downcase
-      break continue if ['y', 'n'].include?(continue)
+      break continue if %w('y', 'n').include?(continue)
     end
     Game.new(human.name).play if continue == 'y'
   end
 end
 
-puts "Play Rock Paper Scissors!"
+puts 'Play Rock Paper Scissors!'
 Game.new.play
-Status API Training Shop Blog About
-© 2016 GitHub, Inc. Terms Privacy Security Contact Help
